0e2983d3768ac67d1b80788a566faec62f240ac2
shalwahm <shalwa.h.meydina@gmail.com>
2024-04-24 10:25:01 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from http import client
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+from http import client
 import requests
 from bs4 import BeautifulSoup
 
-client = MongoClient('mongodb+srv://shalwahmeydina:wawa@shalwa.se90sr2.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
